cee3a68dcbaec6cccfb3b57c175e12591e2cb1d5
Casprine Assempah <casprine.001@gmail.com>
2020-03-29 12:47:13 +0000

    feat: added prettier config

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('boop');
   }
-  return a + b;
+  return a + b * 2;
 };
--- test/blah.test.ts
+++ test/blah.test.ts
@@ -2,6 +2,6 @@ import { sum } from '../src';
 
 describe('blah', () => {
   it('works', () => {
-    expect(sum(1, 1)).toEqual(2);
+    expect(sum(1, 1)).toEqual(4);
   });
 });
