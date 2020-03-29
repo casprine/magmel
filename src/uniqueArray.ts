07265d61a46edc6cb365df0f27bad48859d2be87
Casprine Assempah <casprine.001@gmail.com>
2020-03-29 13:33:13 +0000

    update: update uniqueArray

--- src/uniqueArray.ts
+++ src/uniqueArray.ts
@@ -1,3 +1,5 @@
 export default function uniqueArray(array: Array<{}>) {
+  if (!Array.isArray(array)) return;
+
   return Array.from(new Set(array.map((item: any) => JSON.stringify(item)))).map((item: any) => JSON.parse(item));
 }
